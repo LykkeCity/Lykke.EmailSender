3f38eb41f4d964096dea48b1dbcb6b2c2e768f76
amigin <amigin@gmail.com>
2017-02-02 21:03:12 +0200

    Add project files.

---
+++ src/Lykke.EmailSender/EmailSenderService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Common;
+
+namespace Lykke.EmailSender
+{
+
+    public class EmailAttachment
+    {
+        public string FileName { get; set; }
+        public string Mime { get; set; }
+        public byte[] Data { get; set; }
+    }
+
+    public class EmailModel
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+        public EmailAttachment[] Attachments { get; set; }
+    }
+
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(EmailModel model);
+    }
+
+
+    public class EmailSenderViaQueue : IEmailSender
+    {
+        private readonly IMessageProducer<EmailModel> _producer;
+
+        public EmailSenderViaQueue(IMessageProducer<EmailModel> producer)
+        {
+            _producer = producer;
+        }
+
+        public async Task SendEmailAsync(EmailModel model)
+        {
+            await _producer.ProduceAsync(model);
+        }
+    }
+}
